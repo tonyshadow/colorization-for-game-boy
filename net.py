3637d08424df3e1bb2400643f3b24f4552a16834
Zhidong Liu <zl2591@columbia.edu>
2018-12-10 20:55:08 -0500

    colorization for game boy games

---
+++ net.py
@@ -0,0 +1,142 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+from bak import cfg
+
+
+def _variable_on_cpu(name, shape, initializer):
+    with tf.device('/cpu:0'):
+        var = tf.get_variable(name, shape, initializer=initializer)
+    return var
+
+
+def _variable_with_weight_decay(name, shape, stddev, wd):
+    var = _variable_on_cpu(name, shape, tf.truncated_normal_initializer(stddev=stddev))
+    if wd is not None:
+        weight_decay = tf.multiply(tf.nn.l2_loss(var), wd, name='weight_loss')
+        tf.add_to_collection('losses', weight_decay)
+    return var
+
+
+def _conv_layer(inputs, kernel_shape, stride, index):
+    with tf.variable_scope('conv_%s' % index) as scope:
+        kernel = _variable_with_weight_decay('weights',
+                                             shape=kernel_shape,
+                                             stddev=5e-2,
+                                             wd=cfg.WEIGHT_DECAY)
+        conv = tf.nn.conv2d(inputs, kernel, [1, stride, stride, 1], padding='SAME')
+        biases = _variable_on_cpu('biases', kernel_shape[3:], tf.constant_initializer(0.0))
+        pre_activation = tf.nn.bias_add(conv, biases)
+        conv_relu = tf.nn.relu(pre_activation, name=scope.name)
+
+        return conv_relu
+
+
+def _deconv_layer(inputs, kernel_shape, stride, index):
+    batch_size, height, width, in_channel = [int(i) for i in inputs.shape]
+    out_channel = kernel_shape[3]
+    kernel_size = [kernel_shape[0], kernel_shape[1], kernel_shape[3], kernel_shape[2]]
+    output_shape = [batch_size, height * stride, width * stride, out_channel]
+    with tf.variable_scope('conv_%s' % index) as scope:
+        kernel = _variable_with_weight_decay('weights',
+                                             shape=kernel_size,
+                                             stddev=5e-2,
+                                             wd=cfg.WEIGHT_DECAY)
+        deconv = tf.nn.conv2d_transpose(inputs, kernel, output_shape, [1, stride, stride, 1], padding='SAME')
+
+        biases = _variable_on_cpu('biases', out_channel, tf.constant_initializer(0.0))
+        pre_activation = tf.nn.bias_add(deconv, biases)
+        deconv_relu = tf.nn.relu(pre_activation, name=scope.name)
+
+        return deconv_relu
+
+
+def inference(inputs):
+    # encoder
+    conv1 = _conv_layer(inputs, [3, 3, 3, 64], 2, 1)
+    conv2 = _conv_layer(conv1, [3, 3, 64, 128], 1, 2)
+    conv3 = _conv_layer(conv2, [3, 3, 128, 128], 2, 3)
+    conv4 = _conv_layer(conv3, [3, 3, 128, 256], 1, 4)
+    conv5 = _conv_layer(conv4, [3, 3, 256, 256], 2, 5)
+    conv6 = _conv_layer(conv5, [3, 3, 256, 512], 1, 6)
+    conv7 = _conv_layer(conv6, [3, 3, 512, 512], 1, 7)
+    conv8 = _conv_layer(conv7, [3, 3, 512, 256], 1, 8)
+
+    # decoder
+    conv9 = _conv_layer(conv8, [3, 3, 256, 128], 1, 9)
+    conv10 = _deconv_layer(conv9, [3, 3, 128, 64], 2, 10)
+    conv11 = _conv_layer(conv10, [3, 3, 64, 64], 1, 11)
+    conv12 = _conv_layer(conv11, [3, 3, 64, 64], 1, 12)
+    conv13 = _deconv_layer(conv12, [3, 3, 64, 32], 2, 13)
+    conv14 = _conv_layer(conv13, [3, 3, 32, 32], 1, 14)
+    conv15 = _deconv_layer(conv14, [3, 3, 32, 3], 2, 15)
+    conv16 = _deconv_layer(conv15, [3, 3, 3, 3], 1, 16)
+
+    return conv16
+
+
+def loss(logits, labels):
+    l2_loss = tf.nn.l2_loss(logits - labels)
+    return l2_loss
+
+
+def _add_loss_summaries(total_loss):
+    # Compute the moving average of all individual losses and the total loss.
+    loss_averages = tf.train.ExponentialMovingAverage(0.9, name='avg')
+    losses = tf.get_collection('losses')
+    loss_averages_op = loss_averages.apply(losses + [total_loss])
+
+    # Attach a scalar summary to all individual losses and the total loss; do the
+    # same for the averaged version of the losses.
+    for l in losses + [total_loss]:
+        # Name each loss as '(raw)' and name the moving average version of the loss
+        # as the original loss name.
+        tf.summary.scalar(l.op.name + ' (raw)', l)
+        tf.summary.scalar(l.op.name, loss_averages.average(l))
+
+    return loss_averages_op
+
+
+def train(total_loss, global_step):
+    # Variables that affect learning rate.
+    num_batches_per_epoch = cfg.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN / cfg.BATCH_SIZE
+    decay_steps = int(num_batches_per_epoch * cfg.NUM_EPOCHS_PER_DECAY)
+
+    # Decay the learning rate exponentially based on the number of steps.
+    lr = tf.train.exponential_decay(cfg.INITIAL_LEARNING_RATE,
+                                    global_step,
+                                    decay_steps,
+                                    cfg.LEARNING_RATE_DECAY_FACTOR,
+                                    staircase=True)
+
+    # tf.summary.scalar('learning_rate', lr)
+
+    # Generate moving averages of all losses and associated summaries.
+    loss_averages_op = _add_loss_summaries(total_loss)
+
+    # Compute gradients.
+    with tf.control_dependencies([loss_averages_op]):
+        opt = tf.train.AdadeltaOptimizer(lr)
+        grads = opt.compute_gradients(total_loss)
+
+    # Apply gradients.
+    apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
+
+    # Add histograms for trainable variables.
+    for var in tf.trainable_variables():
+        tf.summary.histogram(var.op.name, var)
+
+    # Add histograms for gradients.
+    for grad, var in grads:
+        if grad is not None:
+            tf.summary.histogram(var.op.name + '/gradients', grad)
+
+    # Track the moving averages of all trainable variables.
+    variable_averages = tf.train.ExponentialMovingAverage(cfg.MOVING_AVERAGE_DECAY, global_step)
+    with tf.control_dependencies([apply_gradient_op]):
+        variables_averages_op = variable_averages.apply(tf.trainable_variables())
+
+    return variables_averages_op
---
+++ train.py
@@ -0,0 +1,189 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import sys
+import time
+from datetime import datetime
+
+import fnmatch
+import tensorflow as tf
+import numpy as np
+from optparse import OptionParser
+
+from bak import cfg, net
+
+sys.path.insert(0, '../utils/')
+
+import feed_dict as fd
+
+FLAGS = tf.flags.FLAGS
+
+tf.flags.DEFINE_integer('max_steps', 100000,
+                        """The weight decay.""")
+
+
+def get_feed_dict(batch_size, original_images_placeholder, gray_images_placeholder, image_list):
+
+    original_images, gray_images = fd.get_batch(batch_size, image_list)
+
+    feed_dict = {
+        original_images_placeholder: original_images,
+        gray_images_placeholder: gray_images
+    }
+
+    return feed_dict
+
+
+# def train_model(checkpoint_dir, image_list, batch_size):
+#         # summaries = tf.summary.scalar('learning_rate', 1)
+#
+#         # summary_op = tf.summary.merge(summaries)
+#
+#         summary_writer = tf.summary.FileWriter(FLAGS.train_dir, sess.graph)
+
+
+def train(checkpoint_dir, image_list):
+    with tf.Graph().as_default():
+        global_step = tf.train.get_or_create_global_step()
+
+        # # # Get images and labels for CIFAR-10.
+        # # # Force input pipeline to CPU:0 to avoid operations sometimes ending up on
+        # # # GPU and resulting in a slow down.
+        # with tf.device('/cpu:0'):
+        #     images, labels = cifar10.distorted_inputs()
+
+        original_images_placeholder = tf.placeholder(tf.float32, shape=(cfg.BATCH_SIZE, 144, 160, 3))
+        gray_images_placeholder = tf.placeholder(tf.float32, shape=(cfg.BATCH_SIZE, 144, 160, 3))
+
+        # image summary for tensorboard
+        tf.summary.image('original_images', original_images_placeholder, max_outputs=100)
+        tf.summary.image('gray_images', gray_images_placeholder, max_outputs=100)
+
+        # Build a Graph that computes the logits predictions from the inference model.
+        logits = net.inference(gray_images_placeholder)
+
+        # Calculate loss.
+        loss = net.loss(original_images_placeholder, logits)
+
+        # # Build a Graph that trains the model with one batch of examples and
+        # # updates the model parameters.
+        # train_op = net.train(loss, global_step)
+
+        # tf.summary.scalar('loss', loss)
+
+        # Create a saver.
+        saver = tf.train.Saver(tf.global_variables())
+
+        train_op = tf.train.AdamOptimizer(learning_rate=1e-5).minimize(loss, global_step=global_step)
+
+        # summary for tensorboard graph
+        # summary_op = tf.summary.merge_all()
+
+        # Build an initialization operation to run below.
+        init = tf.global_variables_initializer()
+        # Start running operations on the Graph. allow_soft_placement must be set to
+        # True to build towers on GPU, as some of the ops do not have GPU
+        # implementations.
+        sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+
+        try:
+            os.mkdir(checkpoint_dir)
+        except:
+            pass
+
+        sess.run(init)
+        print("\nRunning session\n")
+
+        # restore previous model if there is one
+        ckpt = tf.train.get_checkpoint_state(checkpoint_dir + "training")
+        if ckpt and ckpt.model_checkpoint_path:
+            print("Restoring previous model...")
+            try:
+                saver.restore(sess, ckpt.model_checkpoint_path)
+                print("Model restored")
+            except:
+                print("Could not restore model")
+                pass
+
+        # Summary op
+        # graph_def = sess.graph.as_graph_def(add_shapes=True)
+        # summary_writer = tf.summary.FileWriter(checkpoint_dir+"training", graph=sess.graph)
+
+        step = int(sess.run(global_step))
+        gstep = step
+        while gstep < FLAGS.max_steps:
+            start_time = time.time()
+            feed_dict = get_feed_dict(cfg.BATCH_SIZE, original_images_placeholder, gray_images_placeholder, image_list)
+            _, loss_value = sess.run([train_op, loss], feed_dict=feed_dict)
+            duration = time.time() - start_time
+
+            assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
+
+            if step % 1 == 0:
+                num_examples_per_step = cfg.BATCH_SIZE
+                examples_per_sec = num_examples_per_step / duration
+                sec_per_batch = duration
+
+                format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
+                              'sec/batch)')
+                print(format_str % (datetime.now(), step, loss_value,
+                                    examples_per_sec, sec_per_batch))
+
+            # if step % 10 == 0:
+            #     summary_str = sess.run(summary_op)
+            #     summary_writer.add_summary(summary_str, step)
+
+            # Save the model checkpoint periodically.
+            if step % 1000 == 0 or (gstep + 1) == FLAGS.max_steps:
+                print("Saving model")
+                saver.save(sess, checkpoint_dir + "training/checkpoint", global_step=global_step)
+
+            step += 1
+            gstep = int(sess.run(global_step))
+
+        return
+
+
+def main(argv=None):
+    parser = OptionParser(usage='usage')
+    parser.add_option('-c', '--checkpoint_dir',          type='str')
+    parser.add_option('-d', '--data_dir', type='str')
+
+    opts, args = parser.parse_args()
+    opts = vars(opts)
+
+    checkpoint_dir = opts['checkpoint_dir']
+    data_dir = opts['data_dir']
+
+    if checkpoint_dir is None:
+        print ("checkpoint_dir is required")
+        exit()
+
+    print()
+    print('checkpoint_dir: ' + str(checkpoint_dir))
+    print('data_dir:       ' + str(data_dir))
+    print()
+
+    pattern = "*resized.png"
+    image_list = list()
+    for d, s, fList in os.walk(data_dir):
+        for filename in fList:
+            if fnmatch.fnmatch(filename, pattern):
+                image_list.append(os.path.join(d, filename))
+
+    print(str(len(image_list)) + ' images...')
+    train(checkpoint_dir, image_list)
+
+
+if __name__ == "__main__":
+
+    if sys.argv[1] == "--help" or sys.argv[1] == "-h" or len(sys.argv) < 2:
+        print()
+        print("-c --checkpoint_dir <str> [path to save the model]")
+        print("-d --data_dir       <str> [path to root image folder]")
+        print()
+        exit()
+
+    tf.app.run()
